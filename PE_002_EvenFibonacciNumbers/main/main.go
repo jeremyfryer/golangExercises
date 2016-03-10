46fc0d7de63ec9e637c8e902c4da8aa061dbac0c
jeremyfryer <fryer.jeremy@gmail.com>
2016-03-10 00:08:34 -0600

    Project Euler problem #2 Even Fibonacci numbers

---
+++ PE_002_EvenFibonacciNumbers/main/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var sum int
+	for i := 1; fib(i)<4000000; i++ {
+
+		if (fib(i)%2 == 0){
+			sum += fib(i)
+		}
+	}
+	fmt.Println(sum)
+}
+
+func fib(x int) int {
+	if x <= 1 {
+		return x
+	} else {
+		return fib(x-1) + fib(x-2)
+	}
+
+}
